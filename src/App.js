5e891e538476b9e34b3ebd8c82d9ae5c146f84f7
darya-ungvari <63539808+darya-ungvari@users.noreply.github.com>
2021-05-05 21:34:12 +0300

    delete fixed state contacts

--- src/App.js
+++ src/App.js
@@ -6,10 +6,7 @@ import Filter from './Components/Filter/Filter';
 
 class App extends React.Component {
   state = {
-    contacts: [{id: 'id-1', name: 'Rosie Simpson', number: '459-12-56'},
-    {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
-    {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
-    {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'}],
+    contacts: [],
     filter: ''
   }
 
@@ -19,6 +16,7 @@ class App extends React.Component {
     if (existedName) {
       alert(`${contact.name} is already in contacts`)
     } else {this.setState({ contacts: [...this.state.contacts, contact]})}
+
 }
 
   filteredContacts = () => {
--- src/Components/Form/Form.js
+++ src/Components/Form/Form.js
@@ -2,7 +2,6 @@ import React from "react";
 import { v4 as uuidv4 } from 'uuid';
 
 class Form extends React.Component {
-
     state = {
         name: '',
         number: ''
